2fafee59e4272c71e2c242ec96093c495ae37eef
toby <toby.fangyuan@gmail.com>
2021-07-02 20:10:30 -0700

    wsl seems cant display tkinter correctly. moving to windows.

---
+++ test.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Hello, worldaa")
+greeting.pack()
+
+window.mainloop() # tells Python to run the Tkinter event loop
+print("DONE")
